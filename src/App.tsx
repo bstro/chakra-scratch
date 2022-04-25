225ba6ad8127ad554cdd39aeeabd60cd1e95fa94
bstro <627290+bstro@users.noreply.github.com>
2022-04-25 14:52:29 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,51 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Button } from "@chakra-ui/button";
+import { Box, Center, Flex, Grid, GridItem } from "@chakra-ui/layout";
+import { Progress } from "@chakra-ui/progress";
+import { useInterval } from "@chakra-ui/react";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [progressAmount, setProgressAmount] = React.useState<number>(0);
+  const [isDownloading, setIsDownloading] = React.useState(false);
+
+  useInterval(
+    () => {
+      if (progressAmount < 100) {
+        setProgressAmount((cur) => cur + 1);
+      } else {
+        setIsDownloading(false);
+      }
+    },
+    isDownloading ? 16 : null
+  );
+
+  const handleToggle = React.useCallback(() => {
+    setIsDownloading((cur) => !cur);
+    if (progressAmount === 100) {
+      setProgressAmount(0);
+    }
+  }, [setIsDownloading, setProgressAmount, progressAmount]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <Grid templateColumns="1fr 1fr" templateRows="1fr" h="100vh" p={10}>
+      <GridItem alignSelf="center" justifySelf="center">
+        <Button onClick={handleToggle}>
+          {isDownloading ? "Stop download" : "Start download"}
+        </Button>
+      </GridItem>
+
+      <GridItem alignSelf="center">
+        <Progress
+          size="lg"
+          rounded="lg"
+          colorScheme={progressAmount === 100 ? "green" : "blue"}
+          value={progressAmount}
+          isAnimated={isDownloading}
+          hasStripe={progressAmount === 100 ? false : true}
+        />
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
